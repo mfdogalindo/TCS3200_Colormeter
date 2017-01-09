b11394dbe8a81e7eb1335989fa1fef4638783030
Manuel Galindo <mfg90@live.com>
2017-01-09 12:50:54 -0500

    Math added

--- Python/TCS3200/Color_curves.py
+++ Python/TCS3200/Color_curves.py
@@ -2,11 +2,11 @@ import TCS3200
 import time
 import turtle
 import matplotlib.pyplot as plt
+import numpy
 
 COLOR_STeP = 0x0F
 FILTER_SAMPLES = 2
-SENSOR_OFFSET = 50000
-
+DARKNESS_LIMIT = 560000
 
 Colormeter = TCS3200.UART('/dev/ttyACM0',115200)
 
@@ -16,11 +16,10 @@ win.setup(600,600)
 win.title("Color meter RGB test")
 win.bgcolor("black")
 
-
 time.sleep(0.5)
 
-def negative_to_zero(value):
-   return (abs(value)+value)/2 
+def negative_to_zero(value): 
+   return (abs(value)+value)/2  
 
 def get_filtred_sample(samples):
    red=0
@@ -29,67 +28,71 @@ def get_filtred_sample(samples):
    clear=0
    for x in range (0,samples):
       data = Colormeter.read()
-      red += negative_to_zero(SENSOR_OFFSET-float(data[1]))
-      green += negative_to_zero(SENSOR_OFFSET-float(data[3]))
-      blue += negative_to_zero(SENSOR_OFFSET-float(data[5]))
-      clear += negative_to_zero(SENSOR_OFFSET-float(data[7]))  
-   return [(red/samples),(green/samples),(blue/samples),(clear/samples)]
+      red += negative_to_zero(DARKNESS_LIMIT-float(data[1]))
+      green += negative_to_zero(DARKNESS_LIMIT-float(data[3]))
+      blue += negative_to_zero(DARKNESS_LIMIT-float(data[5]))
+      clear += negative_to_zero(DARKNESS_LIMIT-float(data[7])) 
+
+   red = red/samples
+   green = green/samples
+   blue = blue/samples
+   clear = clear/samples
+
+   return [red,green,blue,clear]
 
 #--------------------- GRAYSCALE SAMPLING ----------------------
 
 grayscale_data = []
-counter =0
+grayscale_samples = []
 
 for color in range (0, 0x100, COLOR_STeP):
    html_color = "#"+'%06x' % (color | color << 8 | color << 16) 
    win.bgcolor(html_color)
-   counter+=1
+   grayscale_samples += [color]
    grayscale_data += [get_filtred_sample(FILTER_SAMPLES)]
 
-grayscale_samples = [i for i in range(counter)]
 
 #--------------------- RED COLOR SAMPLING ----------------------
-red_data = []   
-counter = 0   
-
+red_data = []  
+red_samples = []
+ 
 for level in range(0, 0x100, COLOR_STeP):
    html_color = "#"+'%06x' % (level << 16) 
    win.bgcolor(html_color)
-   counter+=1
+   red_samples += [level]
    red_data += [get_filtred_sample(FILTER_SAMPLES)]
-
-red_samples = [i for i in range(counter)]  
 
 #--------------------- GREEN COLOR SAMPLING ---------------------
 green_data = []   
-counter = 0   
+green_samples = []
 
 for level in range(0, 0x100, COLOR_STeP):
    html_color = "#"+'%06x' % (level << 8) 
    win.bgcolor(html_color)
-   counter+=1
+   green_samples += [level]
    green_data += [get_filtred_sample(FILTER_SAMPLES)]
 
-green_samples = [i for i in range(counter)]  
+
 
 #--------------------- BLUE COLOR SAMPLING ----------------------
 blue_data = []   
-counter = 0   
+blue_samples = [] 
 
 for level in range(0, 0x100, COLOR_STeP):
    html_color = "#"+'%06x' % (level) 
    win.bgcolor(html_color)
-   counter+=1
+   blue_samples += [level]
    blue_data += [get_filtred_sample(FILTER_SAMPLES)]
-
-blue_samples = [i for i in range(counter)]  
 
 #------------------------- PLOTTING --------------------------
 consolidated_red=[]
 consolidated_green=[]
 consolidated_blue=[]
+consolidated_clear=[]
+minimal = []
+maximum = []
 
-plt.figure(1)
+plt.figure("RAW DATA")
 
 for figure_num in range (0,4):
    data=[]
@@ -118,27 +121,55 @@ for figure_num in range (0,4):
       title = "Blue"
      
 
-   for x in samples:
+   for x in range (0,len(samples)):
       red += [data[x][0]]
       green += [data[x][1]]
       blue += [data[x][2]]
       clear += [data[x][3]]
-   
+
+
+   if figure_num is 0:
+      consolidated_clear = clear
+      minimal += [min(clear)]
+      maximum += [max(clear)] 
    if figure_num is 1:
       consolidated_red = red
+      minimal += [min(red)]
+      maximum += [max(red)]   
+   if figure_num is 1:
+      consolidated_red = red
+      minimal += [min(red)]
+      maximum += [max(red)]
    if figure_num is 2:
       consolidated_green = green
+      minimal += [min(green)]
+      maximum += [max(green)]
    if figure_num is 3:
       consolidated_blue = blue
+      minimal += [min(blue)]
+      maximum += [max(blue)]
 
    plt.subplot(2,2,figure_num+1)
    plt.title(title)
    plt.plot(samples,red,'r',samples,green,'g',samples,blue,'b',samples,clear,'b--')
+   plt.xlim(0,0xFF)
    plt.gcf().autofmt_xdate()
 
-plt.figure(2)  
-plt.title("Consolidated")
-plt.plot(samples,consolidated_red,'r',samples,consolidated_green,'g',samples,consolidated_blue,'b')
+#---------------------- NORMALIZED PLOT ------------------------
+   
+max=max(maximum)
+min=min(minimal)
+
+
+consolidated_red = (numpy.asarray(consolidated_red))/max
+consolidated_green = (numpy.asarray(consolidated_green))/max
+consolidated_blue =  (numpy.asarray(consolidated_blue))/max   
+consolidated_clear =  (numpy.asarray(consolidated_clear))/max 
+
+
+plt.figure("NORMALIZED")  
+plt.plot(samples,consolidated_red,'r',samples,consolidated_green,'g',samples,consolidated_blue,'b',consolidated_clear,'c--')
+plt.xlim(0,0xFF)
 plt.gcf().autofmt_xdate() 
 
 plt.show()
